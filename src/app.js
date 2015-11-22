bd2ec2b62f44c0426fb06762e1d15b10c7716ce9
Pavel Horal <pavel.horal@orchitech.cz>
2015-11-22 14:25:29 +0100

    Group parsing... still not there yet, but close.

--- src/app.js
+++ src/app.js
@@ -52,18 +52,36 @@ angular.module('bethparser', []).
     factory('modParse', function() {
         var readRecord = function(sourceData, offset) {
             var	sourceView = new DataView(sourceData, offset),
-                type = String.fromCharCode(
-                        sourceView.getUint8(0), sourceView.getUint8(1),
-                        sourceView.getUint8(2), sourceView.getUint8(3)),
                 record = {
-                    type: type,
+                    type: String.fromCharCode(
+                            sourceView.getUint8(0), sourceView.getUint8(1),
+                            sourceView.getUint8(2), sourceView.getUint8(3)),
                     size: sourceView.getUint32(4, true),
                     flags: sourceView.getUint32(8, true),
                     id: sourceView.getUint32(12, true),
                     revision: sourceView.getUint32(16, true),
                     version: sourceView.getUint16(20, true)
                 };
+            if (record.type === 'GRUP') {
+                record.data = readGroup(sourceData, offset + 24);
+            }
             return record;
+        };
+        var readGroup = function(sourceData, offset) {
+            var sourceView = new DataView(sourceData, offset),
+                groupData = {
+                    size: sourceView.getUint32(4, true),
+                    type: String.fromCharCode(
+                            sourceView.getUint8(0), sourceView.getUint8(1),
+                            sourceView.getUint8(2), sourceView.getUint8(3)),
+                    records: []
+                },
+                readOffset = 24;
+            while (readOffset < groupData.size) {
+                groupData.records.push(readRecord(sourceData, readOffset));
+                readOffset += 24 + groupData.records[groupData.records.length - 1].size;
+            };
+            return groupData;
         };
         return function(sourceData) {
             var resultData = {
@@ -116,11 +134,20 @@ angular.module('bethparser', []).
         };
 
         this.renderMod = function() {
-            var content = '';
+            this.data.$content = '';
+            var renderRecord = function(indent, record) {
+                var result = '[' + record.type + '] ' + '[' + hex4(record.id) + ']';
+                if (record.type === 'GRUP') {
+                    result += ' [' + record.data.type + ']';
+                    record.data.records.forEach(function(subrecord) {
+                        result += '\n' + renderRecord(indent + '    ', subrecord);
+                    });
+                }
+                return indent + result;
+            };
             this.data.records.forEach(angular.bind(this, function(record) {
-                content += '[' + record.type + '] ' + '[' + hex4(record.id) + ']\n';
+                this.data.$content += renderRecord('', record) + '\n';
             }));
-            this.data.$content = content;
         };
 
     });
